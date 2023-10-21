392db5a4ea7e0fad9fa76fb3f5a1ca7b6f060a25
Dhruv <dhruvvaishnav2125@gmail.com>
2023-10-21 18:53:28 +0530

    put the sensitive data in .env file and using the WSGI server  for that configured the Procfile .

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 from sqlalchemy.orm import relationship  # Import the relationship function
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from decouple import config
 from forms import *
 
 from functools import wraps
@@ -30,7 +30,7 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -55,7 +55,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = config('DB_URI', default='sqlite:///posts.db')
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -298,4 +298,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
